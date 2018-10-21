b28565544e47810d1c1dd4d3deaf58a27e706147
Vilius <viliusgrizas@gmail.com>
2018-10-21 10:52:13 +0300

    added comments just to form habbits

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 var maxNumber = 300	;
-var number = 1 + Math.floor(Math.random() * maxNumber);
+var number = 1 + Math.floor(Math.random() * maxNumber); //generate random number
 console.log("selected number is "+number);
 
-// function printNumbers(){
-// 	for (var i = 1; i<=maxNumber; i++){
-// 		$(".numbers").append("<span class='n"+i+"' value='"+i+"'>"+i+" </span>");
-// 	}
-// }
+
 function guessRandomNumber(){
-	var minNumber=0;
-	var guessNo = 1;
+	var minNumber=0; 
+	var guessNo = 1; //number of guess 
 	
 	while(guess!==number){
-		var guess = Math.floor((minNumber+maxNumber)/2);
+		var guess = Math.floor((minNumber+maxNumber)/2); 
 		console.log("My "+guessNo+" guess is "+guess);
+		//check if number is higher or lower and set variables accordingly
 		if(guess>number)
 		{
 			console.log("it's lower");
