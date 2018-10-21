94b473a98829dd6ba990ccb2b8a25684cf3fd38d
Vilius <viliusgrizas@gmail.com>
2018-10-21 11:07:37 +0300

    added some logic so that guessing wouldn't include numbers you've already guessed

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-var maxNumber = 300	;
+var maxNumber = 300;
 var number = 1 + Math.floor(Math.random() * maxNumber); //generate random number
 console.log("selected number is "+number);
 
 
 function guessRandomNumber(){
-	var minNumber=0; 
+	var minNumber=1; 
 	var guessNo = 1; //number of guess 
 	
 	while(guess!==number){
+		console.log("now I'm guessing from "+minNumber+" to "+maxNumber);
 		var guess = Math.floor((minNumber+maxNumber)/2); 
-		console.log("My "+guessNo+" guess is "+guess);
+		console.log(guessNo+") My guess is "+guess);
 		//check if number is higher or lower and set variables accordingly
 		if(guess>number)
 		{
 			console.log("it's lower");
-			maxNumber=guess;
+			maxNumber=guess-1;
 		} else if(guess<number)
 		{
 			console.log("it's higher");
-			minNumber=guess;
+			minNumber=guess+1;
 		}
 
 		if(guess==number)
