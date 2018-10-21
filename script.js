b12cc6e65d04663d70ea6a8b1e3082427ba0f07a
Vilius <viliusgrizas@gmail.com>
2018-10-21 10:49:38 +0300

    Finished. No front end, but the goal was to make the logic. Moving on

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-var maxNumber = 300;
+var maxNumber = 300	;
 var number = 1 + Math.floor(Math.random() * maxNumber);
 console.log("selected number is "+number);
 
-function printNumbers(){
-	for (var i = 1; i<=maxNumber; i++){
-		$(".numbers").append("<span class='n"+i+"' value='"+i+"'>"+i+" </span>");
-	}
-}
+// function printNumbers(){
+// 	for (var i = 1; i<=maxNumber; i++){
+// 		$(".numbers").append("<span class='n"+i+"' value='"+i+"'>"+i+" </span>");
+// 	}
+// }
 function guessRandomNumber(){
 	var minNumber=0;
+	var guessNo = 1;
 	
-	for(var i=0;i<9;i++){
+	while(guess!==number){
 		var guess = Math.floor((minNumber+maxNumber)/2);
-		console.log("my guess is "+guess);
+		console.log("My "+guessNo+" guess is "+guess);
 		if(guess>number)
 		{
 			console.log("it's lower");
@@ -27,6 +28,6 @@ function guessRandomNumber(){
 		{
 			console.log("congratz");
 		}
-		console.log("min:"+minNumber+"max:"+maxNumber)
+		guessNo++;
 	}
 }
